f6ac4f3d6783ea2d9388986892506c7958d71005
Manuel <lehnerma@fim.uni-passau.de>
2022-05-12 10:47:40 +0200

    added encrypted files for authentication start of user registration implementation

--- telegrambot.py
+++ telegrambot.py
@@ -1,5 +1,8 @@
 import logging
+import random
 
+import grafana_api.grafana_api
+import string
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (
     CallbackContext,
@@ -9,6 +12,13 @@ from telegram.ext import (
     filters,
     Application
 )
+from os.path import exists as file_exists
+from cryptography.fernet import Fernet
+
+
+def id_generator(size=12, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 
 # Enable logging
 logging.basicConfig(
@@ -16,37 +26,78 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-GENDER, PHOTO, LOCATION, BIO = range(4)
+MENU, PHOTO, LOCATION, BIO, REGISTER = range(5)
 
 from grafana_api.grafana_face import GrafanaFace
 
-def auth_grafana():
-    grafana_api = GrafanaFace(
-        auth=("username", "password"),
-        host='api.energy.uni-passau.de:8080'
-    )
+global encrypter
+
+
+def auth_grafana(host='api.energy.uni-passau.de:8080') -> GrafanaFace:
+    with open('graf.auth') as auth_file:
+        username = auth_file.readline()
+        password = auth_file.readline()
+        return GrafanaFace(
+            auth=(username, password),
+            host=host
+        )
 
 
 async def start(update: Update, callback: CallbackContext.DEFAULT_TYPE) -> int:
     """Starts the conversation and asks the user about their gender."""
-    reply_keyboard = [["Boy", "Girl", "Other"]]
+    reply_keyboard_register = [["Yes", "No"]]
+    reply_keyboard_menu = [["Stats today", "Devices", "Help"]]
+    user = update.message.from_user
 
-    await update.message.reply_text(
-        "Hi! My name is Professor Bot. I will hold a conversation with you. "
-        "Send /cancel to stop talking to me.\n\n"
-        "Are you a boy or a girl?",
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Boy or Girl?"
-        ),
-    )
+    await  update.message.reply_text(f"Hello {user.first_name}, welcome to the RPMT notification beta application "
+                                     "Send /cancel to stop talking to me.")
 
-    return GENDER
+    if not file_exists(f'user/{update.message.from_user.id}'):
+        await update.message.reply_text(
+            "Apparently you have not yet used our service.\n\n"
+            "Do you want to register an account?",
+            reply_markup=ReplyKeyboardMarkup(reply_keyboard_register,
+                                             one_time_keyboard=True,
+                                             input_field_placeholder="Choose an item"
+                                             ),
+        )
+        return REGISTER
+    else:
+        await  update.message.reply_text(f"How can I help you today",
+                                         reply_markup=ReplyKeyboardMarkup(reply_keyboard_menu,
+                                                                          one_time_keyboard=True,
+                                                                          input_field_placeholder="Choose an item"
+                                                                          )
+                                         )
+        return MENU
 
 
-async def gender(update: Update, callback: CallbackContext.DEFAULT_TYPE) -> int:
+async def register(update: Update, callback: CallbackContext.DEFAULT_TYPE) -> int:
+    """Creates a new user using the grafana api, and saves user credentials in an id folder"""
+    grafana = auth_grafana()
+    user_login_id = id_generator()
+    user_login_pw = id_generator()
+    with open(f'users/{update.message.from_user.id}', 'wb') as cred_file:
+        cred_file.write(encrypter.encrypt(user_login_id))
+        cred_file.write(encrypter.encrypt(user_login_pw))
+        cred_file.close()
+
+    new_user = {
+        "name": f"{update.message.from_user.full_name}",
+        "email": "user@graf.com",
+        "login": f'{user_login_id}',
+        "password": f'{user_login_pw}',
+        "OrgId": 1
+    }
+    grafana.admin.create_user(new_user)
+
+    return MENU
+
+
+async def menu(update: Update, callback: CallbackContext.DEFAULT_TYPE) -> int:
     """Stores the selected gender and asks for a photo."""
     user = update.message.from_user
-    logger.info("Gender of %s: %s", user.first_name, update.message.text)
+    logger.info("User %s entered to %s", user.first_name, update.message.text)
     await update.message.reply_text(
         "I see! Please send me a photo of yourself, "
         "so I know what you look like, or send /skip if you don't want to.",
@@ -127,6 +178,29 @@ async def cancel(update: Update, callback: CallbackContext.DEFAULT_TYPE) -> int:
 
 def main() -> None:
     """Run the bot."""
+    global encrypter
+    if not file_exists('k.ey'):
+        key = Fernet.generate_key()
+        with open("k.ey", "wb") as key_file:
+            key_file.write(key)
+            encrypter = Fernet(key)
+    else:
+        with open("k.ey", "rb") as key_file:
+            key = key_file.read()
+            encrypter = Fernet(key)
+
+    if not file_exists('graf.auth'):
+        with open("graf.auth", 'wb') as auth_file:
+            username_graf = password_graf = b'admin'
+            encr_username = encrypter.encrypt(username_graf)
+            encr_password = encrypter.encrypt(password_graf)
+            auth_file.write(encr_username)
+            auth_file.write(encr_password)
+    else:
+        with open('graf.auth', 'rb') as auth_file:
+            username_graf = encrypter.decrypt(auth_file.readline())
+            password_graf = encrypter.decrypt(auth_file.readline())
+
     # Create the Application and pass it your bot's token.
     with open("token") as file:
         token = file.read()
@@ -136,13 +210,16 @@ def main() -> None:
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
-            GENDER: [MessageHandler(filters.Regex("^(Boy|Girl|Other)$"), gender)],
+            MENU: [MessageHandler(filters.Regex("^(Register|LogIn|Other)$"), menu)],
+
             PHOTO: [MessageHandler(filters.PHOTO, photo), CommandHandler("skip", skip_photo)],
             LOCATION: [
                 MessageHandler(filters.LOCATION, location),
                 CommandHandler("skip", skip_location),
             ],
             BIO: [MessageHandler(filters.TEXT & ~filters.COMMAND, bio)],
+            REGISTER: [MessageHandler(filters.Regex("Yes"), register),
+                       MessageHandler(filters.Regex("No"), cancel)]
         },
         fallbacks=[CommandHandler("cancel", cancel)],
     )
